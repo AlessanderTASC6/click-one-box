687a8ff6317f8da4a70d2eb294f80cdc8bedaaf1
AlessanderTASC6 <attu72289@gmail.com>
2019-07-16 09:07:21 -0400

    Blue to red when mouse  is clicked

---
+++ sketch.js
@@ -0,0 +1,26 @@
+// 
+let value = 0
+function setup(){
+    createCanvas(600,600);
+    background(0, 255, 0);
+}
+
+function draw(){
+    square(0,0,600)
+    fill(color(0,0,255))
+   
+}
+
+let clicks = 0
+
+function mouseClicked(){
+    square(0,0,600)
+    fill('rgb(100%,0%,10%)')
+}
+
+
+
+
+
+// function Click(){}
+    
